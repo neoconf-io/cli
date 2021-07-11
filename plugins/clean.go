09b0213138f7ed499d6f8a82f4cf17370d165ca3
Andrej Benz <hello@benz.dev>
2021-07-11 12:51:48 +0200

    clean now also removes orphaned plugin folders

--- plugins/clean.go
+++ plugins/clean.go
@@ -9,7 +9,7 @@ import (
 	"github.com/abenz1267/neoconf/structure"
 )
 
-// Clean removes plugin configs for missing plugins.
+// Clean removes orphaned plugin configs and folders.
 func Clean() {
 	f, err := ioutil.ReadDir(structure.Dir.PluginCfg)
 	if err != nil {
@@ -41,8 +41,40 @@ func Clean() {
 		}
 	}
 
+	s, err := ioutil.ReadDir(structure.Dir.PStart)
+	if err != nil {
+		panic(err)
+	}
+
+	pf := []string{}
+
+	for _, c := range s {
+		found := false
+
+		for _, v := range p {
+			if string(v.repo.dir()) == c.Name() {
+				found = true
+
+				break
+			}
+		}
+
+		if !found {
+			pf = append(pf, c.Name())
+		}
+	}
+
 	for _, v := range d {
 		err := os.Remove(filepath.Join(structure.Dir.PluginCfg, v))
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("Removed '%s'\n", v)
+	}
+
+	for _, v := range pf {
+		err := os.RemoveAll(filepath.Join(structure.Dir.PStart, v))
 		if err != nil {
 			panic(err)
 		}
